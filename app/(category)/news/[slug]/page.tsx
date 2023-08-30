645965d6a1dfdb61e104b2114eda73492870f565
mkropidlowski <mkropidlowsky@gmail.com>
2023-08-30 18:34:11 +0200

    fix: fix slug category functionality

--- app/(category)/news/[slug]/page.tsx
+++ app/(category)/news/[slug]/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/hero/hero";
 import { getByCategories } from "@/app/modules/getByCategories";
 
+const LIMIT = 10;
+
 const NewsPage = async ({ params }: { params: { slug: string } }) => {
     const slug = params.slug;
-    const newsByCategory = await getByCategories(slug, 10);
+    const newsByCategory = await getByCategories(LIMIT, slug);
 
-    // return <Hero newsData={newsByCategory} />;
+    return <Hero newsData={newsByCategory} />;
 };
 
 export default NewsPage;
